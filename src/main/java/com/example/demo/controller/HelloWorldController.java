e6d1ae1d3d292472aff238c19392a3bb49b548b4
rudani07 <118912088+rudani07@users.noreply.github.com>
2024-02-24 23:33:47 -0500

    USed requestparam annotation and get data fetched from one page to another web page

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -22,6 +23,13 @@ public class HelloWorldController {
         String theName = request.getParameter("studentName");
         theName = theName.toUpperCase();
         String result = "yo!" + theName;
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+        theName = theName.toUpperCase();
+        String result = "Hey my friend!" + theName;
         model.addAttribute("message",result);
         return "helloworld";
     }
