03afe4e0911c1608bff243ab6c32e8eb46f12bf0
rudani07 <118912088+rudani07@users.noreply.github.com>
2024-02-26 15:17:28 -0500

    Data transfer with major input types from one page to another.

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,8 +1,16 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
+    private String favouriteLanguage;
+
+    private List<String> favouriteSystems;
+
+
 
     public Student() {
     }
@@ -18,8 +26,32 @@ public class Student {
         return lastName;
     }
 
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public List<String> getFavouriteSystems() {
+        return favouriteSystems;
+    }
+
+    public void setFavouriteSystems(List<String> favouriteSystems) {
+        this.favouriteSystems = favouriteSystems;
+    }
 }
